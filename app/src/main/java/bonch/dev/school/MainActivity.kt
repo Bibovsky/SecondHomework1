cef622a422068085f9fe79904f4fa10c97484f27
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-10-16 19:18:17 +0300

    indic

--- app/src/main/java/bonch/dev/school/MainActivity.kt
+++ app/src/main/java/bonch/dev/school/MainActivity.kt
@@ -1,12 +1,32 @@
 package bonch.dev.school
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var indButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
+        setListeners()
+    }
+    private fun initializeViews(){
+        indButton = findViewById(R.id.indicator_button)
+
+
+    }
+    private fun setListeners(){
+        indButton.setOnClickListener{
+            indButton.isClickable=false
+        }
+        /*nextActButton.setOnClickListener(){
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }*/
+
     }
 }
