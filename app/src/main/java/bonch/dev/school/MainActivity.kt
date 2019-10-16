d4d94ae5c80eedada551fa3b8ac639aba252ad64
Bibovsky <48915487+Bibovsky@users.noreply.github.com>
2019-10-16 19:25:15 +0300

    counter

--- app/src/main/java/bonch/dev/school/MainActivity.kt
+++ app/src/main/java/bonch/dev/school/MainActivity.kt
@@ -7,7 +7,8 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var indButton: Button
-
+    private lateinit var countButton: Button
+    var a=0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,13 +17,20 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initializeViews(){
         indButton = findViewById(R.id.indicator_button)
+        countButton = findViewById(R.id.counter_button)
 
 
     }
     private fun setListeners(){
+        var a=0
         indButton.setOnClickListener{
             indButton.isClickable=false
         }
+        countButton.setOnClickListener{
+            a++
+            countButton.text=a.toString()
+        }
+
         /*nextActButton.setOnClickListener(){
             val intent = Intent(this,SecondActivity::class.java)
             startActivity(intent)
